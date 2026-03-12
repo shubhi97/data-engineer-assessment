7bb9608589eccb1ead2662d530397473e9addc77
Shubhi Jain <shubhi.it.jain@gmail.com>
2026-03-12 15:49:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
-```
-
-Click **Commit changes**
-
----
-
-## File 5: `src/App.jsx`
-Create new file, name it `src/App.jsx` and paste the **entire contents** of the `DataEngineerInterview.jsx` file I gave you earlier.
-
-Click **Commit changes**
-
----
-
-## File 6: The GitHub Actions Workflow
-Create new file, name it exactly:
-```
-.github/workflows/deploy.yml
